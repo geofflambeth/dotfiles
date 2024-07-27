15b78c37188e6a6604e6eb377943747f1696300f
Geoffrey Lambeth <geoffrey.lambeth@nau.edu>
2024-07-27 11:32:46 -0700

    run install script from any folder with symlinks

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-# copy nvim config files
-cp -r ./nvim $HOME/.config/nvim
+# identify directory of install script
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+# link all dotfiles to config folder
+ln -s $DIR/nvim $HOME/.config/nvim
