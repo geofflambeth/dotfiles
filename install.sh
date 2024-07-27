98e5f89853f1ecd81324110dcf51ce2c4b90b767
Geoffrey Lambeth <geoffrey.lambeth@nau.edu>
2024-07-27 11:54:42 -0700

    test

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ### NOTES ###
 # Keep .dotfiles repo at $HOME/.dotfiles or use $DEPLOY_HOME env variable 
+echo "this is a test"
 if [[ -z "$(DEPLOY_HOME)" ]]; then
   INSTALL_DIR="$(HOME)/.dotfiles" # get install dir from $HOME because $DEPLOY_HOME is not set
 else
