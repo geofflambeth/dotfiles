5f58b87458415b3bf98ff6e8690dc5ef3e4788b4
Geoffrey Conley <geoffrey@violetsquid.xyz>
2024-11-27 19:37:03 +0000

    wip -- basic linux installation working

--- install.sh
+++ install.sh
@@ -1,33 +1,43 @@
 #!/usr/bin/env bash
 
 ### NOTES ###
-# Keep dotfiles repo at $HOME/.dotfiles or use $DEPLOY_HOME env variable 
+# Run from within the dotfiles folder for relative filepaths to work
 
-if [[ -z "${DEPLOY_HOME}" ]]; then
-  INSTALL_DIR="${HOME}/dotfiles" # get install dir from $HOME because $DEPLOY_HOME is not set
-else
-  INSTALL_DIR="${DEPLOY_HOME}"
-fi
+DOTFILES_ROOT=$PWD
+DOTFILES_INSTALLATION_DIR=$HOME/.dotfiles-installation
+rm -r $DOTFILES_INSTALLATION_DIR
+mkdir $DOTFILES_INSTALLATION_DIR
 
 # If linux, install linux stuff
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+
+  # neovim:latest via appimage
+  mkdir $DOTFILES_INSTALLATION_DIR/nvim
+  cd $DOTFILES_INSTALLATION_DIR/nvim
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
   ./nvim.appimage --appimage-extract
-  ./squashfs-root/AppRun --version
-  ln -s /squashfs-root/AppRun /usr/bin/nvim
+  rm /usr/local/bin/nvim
+  ln -s $DOTFILES_INSTALLATION_DIR/nvim/squashfs-root/AppRun /usr/local/bin/nvim
+  nvim --version
 
   # Install lua itself
+  mkdir $DOTFILES_INSTALLATION_DIR/lua
+  cd $DOTFILES_INSTALLATION_DIR/lua
   curl -R -O https://www.lua.org/ftp/lua-5.3.5.tar.gz
   tar -zxf lua-5.3.5.tar.gz
   cd lua-5.3.5 && make linux test && make install
   
   # Install luarocks pkg manager for neovim
+  mkdir $DOTFILES_INSTALLATION_DIR/luarocks
+  cd $DOTFILES_INSTALLATION_DIR/luarocks
   curl -R -O http://luarocks.github.io/luarocks/releases/luarocks-3.11.1.tar.gz
   tar -zxf luarocks-3.11.1.tar.gz
   cd luarocks-3.11.1 && ./configure --with-lua-include=/usr/local/include && make && make install
   
-  # Install pandoc 2.19.2
+  # Install pandoc 2.19.2 -- this is the specific version I prefer, due to compatibility with the pandoc python package
+  mkdir $DOTFILES_INSTALLATION_DIR/pandoc && cd $DOTFILES_INSTALLATION_DIR/pandoc
   curl -LO https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-1-amd64.deb
   dpkg -i pandoc*.deb && rm *.deb
 fi
@@ -36,5 +46,5 @@ fi
 # create main .config folder
 mkdir -p $HOME/.config
 # install neovim configs
-ln -sfn $INSTALL_DIR/nvim $HOME/.config/nvim
+ln -sfn ./nvim $HOME/.config/nvim
 nvim --headless "+Lazy! sync" +qa
